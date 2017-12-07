ba4178420f6a3e0a135faeec7a0e87b8497889a2
Folk <vhaarr@gmail.com>
2017-12-07 17:12:01 +0100

    Bump to 1.9.x.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Auto Auto-Explore",
 	tags = { "Gameplay", "Technologies" },
 	picture = "thumb.png",
-	supported_version = "1.8.*",
+	supported_version = "1.9.*",
 	remote_file_id = 785299768,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
